4dceeee31d84c3bd8964cab621fa31cc75be9405
JovliyevOlim <olimjovliyev98@gmail.com>
2023-01-11 10:16:49 +0500

    url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,6 @@
-// const  baseUrl = 'https://shopping-cart-rest-server.herokuapp.com/api'
-const baseUrl = 'http://localhost:2000/api'
+const  baseUrl = 'https://shopping-cart-rest-server.herokuapp.com/api'
+// const baseUrl = 'http://localhost:2000/api'
 export const api = baseUrl
 export const generatePublicUrl =  (filename)=>{
-    return `${baseUrl}/public/${filename}`
+    return `${filename}`
 }
